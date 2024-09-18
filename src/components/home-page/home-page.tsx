8720bd38d3617b9d58cd46d28978bc705f03cd65
ananya_singh <singhananya985@gmail.com>
2024-09-19 01:48:39 +0530

    basic functionality to switch b/w card and button

--- src/components/home-page/home-page.tsx
+++ src/components/home-page/home-page.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { Home } from "../styles/common-styles";
 import useBoardsContext from "../context/useBoardsContext";
-import CustomAddButton from "../../utils/custom-add-button";
+import AddListOrCard from "./add-list-card";
 
 // Under Development
 const HomePage: React.FC = () => {
@@ -9,6 +9,7 @@ const HomePage: React.FC = () => {
     state: { currentActiveBoard, boards },
   } = useBoardsContext();
   const [toAddList, setIsToAddList] = useState(false);
+
   return (
     <Home>
       <div>
@@ -23,18 +24,12 @@ const HomePage: React.FC = () => {
               </div>
             ))}
       </div>
-      <div>
-        {toAddList ? (
-          <CustomAddButton
-            title={boards.length ? "Add another list" : "Add a llist"}
-            handleClick={() => setIsToAddList(true)}
-          />
-        ) : (
-          <div onClick={() => setIsToAddList(false)} className="custom-btn">
-            Card to create a list
-          </div>
-        )}
-      </div>
+      <AddListOrCard
+        toAdd={toAddList}
+        title={boards.length ? "Add another list" : "Add a llist"}
+        handleAdd={() => setIsToAddList(false)}
+        handleClose={() => setIsToAddList(true)}
+      />
     </Home>
   );
 };
